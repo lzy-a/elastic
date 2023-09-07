c3ec3eef40d73caa7e347040e90b7bb9a6eb0f71
lzy-a <1160440131@qq.com>
2023-09-07 11:20:03 +0800

    flush

--- train_elastic.py
+++ train_elastic.py
@@ -39,7 +39,7 @@ class KafkaDataset(torch.utils.data.Dataset):
         input_data = torch.tensor([float(d) for d in data[:10]]).cuda(local_rank)
         labels = torch.tensor([float(d) for d in data[10:]]).cuda(local_rank)
         timestamp = message.timestamp
-        proc_file.write(f"-------get-item-span = {time.time() - start}\n")
+        proc_file.write(f"[{os.getpid()}]-------get-item-span = {time.time() - start}\n")
         proc_file.flush()
         return {
             'input_data': input_data,
@@ -106,19 +106,22 @@ def train():
             print(f"[{os.getpid()}] Received labels: {labels}")
             lag = time.time() - timestamp
             lag_file.write(f"cur: {time.time()}, timestamp: {timestamp}, Lag: {lag}\n")
+            lag_file.flush()
             start = time.time()
             optimizer.zero_grad()
             outputs = ddp_model(input_data)  # 输入数据要进行维度扩展
             loss = loss_fn(outputs, labels)
             loss.backward()
             proc_file.write(f"epoch {i} grad-span = {time.time()-start}\n")
+            proc_file.flush()
             print(f"[{os.getpid()}] epoch {i} (rank = {rank}, local_rank = {local_rank}) loss = {loss.item()}\n")
             start = time.time()
             optimizer.step()
             proc_file.write(f"epoch {i} sync-span = {time.time() - start}\n")
-            if i % 10 == 0:
-                lag_file.flush()
-                proc_file.flush()
+            proc_file.flush()
+            # if i % 10 == 0:
+                # lag_file.flush()
+                # proc_file.flush()
             save_checkpoint(i, ddp_model, optimizer, ckp_path)
             i += 1
 
