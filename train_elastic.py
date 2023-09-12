5463126389b4901d71ca1a9de14f435cd9c6dca1
lzy-a <1160440131@qq.com>
2023-09-12 15:50:39 +0800

    print

--- train_elastic.py
+++ train_elastic.py
@@ -16,12 +16,13 @@ from torch.utils.data import DataLoader
 from torch.nn.parallel import DistributedDataParallel as DDP
 from kafka import KafkaConsumer
 
+
 # 设置 Kafka 主题和服务器地址
 bootstrap_servers = '11.32.251.131:9092,11.32.224.11:9092,11.32.218.18:9092'
 topic = 'stream-6'
 # 创建 Kafka 消费者
 consumer = KafkaConsumer(topic, bootstrap_servers=bootstrap_servers, group_id='1', auto_offset_reset='latest')
-
+print(f"[{os.getpid()}] ( consumer starting...")
 lag_file = open('lag.txt', 'w')
 proc_file = open('proc.txt', 'w')
 
