68b02e234381ddac0439525ab5c8f6092c2d32f1
lzy-a <1160440131@qq.com>
2023-09-15 13:38:42 +0800

    global batch

--- train_elastic.py
+++ train_elastic.py
@@ -94,7 +94,7 @@ def train():
         first_epoch = checkpoint["epoch"]
 
     # 创建数据加载器
-    batch_size = global_batch_size / world_size
+    batch_size = int(global_batch_size / world_size)
     dataset = KafkaDataset()
     # sampler = torch.utils.data.distributed.DistributedSampler(dataset, num_replicas=world_size,
     #                                                           rank=rank)
