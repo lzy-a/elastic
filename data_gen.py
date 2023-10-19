54f09b534a834de8c8e7625878b05d98f168ac29
lzy-a <1160440131@qq.com>
2023-10-19 14:26:57 +0800

    data

--- data_gen.py
+++ data_gen.py
@@ -1,26 +1,27 @@
 import numpy as np
 import pandas as pd
 
-# 定义时间段和对应的流量
-time_periods = ['1-7', 'other']
-traffic_data = [1000, 6000]
 
 # 定义全天的24小时
 hours = np.arange(24)
 
-# 生成基础流量数据
-base_traffic = np.zeros_like(hours)
-for i, period in enumerate(time_periods):
-    if period == '1-7':
-        base_traffic[hours < 7] = traffic_data[i]
+# 设置一年日期范围
+date_range = pd.date_range(start='1/1/2023', end='12/31/2023')
+
+# 初始化一个空的DataFrame用于存储流量数据
+traffic_data = pd.DataFrame(index=date_range)
+
+# 为每天的1-7点设置流量为1000，其他时间段流量为6000
+for hour in range(24):
+    if hour < 7:
+        traffic_data.loc[date_range, hour] = 1000
     else:
-        base_traffic[hours >= 7] = traffic_data[i]
+        traffic_data.loc[date_range, hour] = 6000
 
-    # 生成带有10%波动性的新数据
-std_dev = 0.1 * base_traffic  # 计算标准差
-traffic_data_new = base_traffic + np.random.normal(0, std_dev)  # 生成新数据
+    # 对全时段应用10%的随机波动
+for hour in range(24):
+    traffic_data.loc[date_range, hour] = traffic_data.loc[date_range, hour] * (1 + np.random.rand(len(date_range)) / 10)
+    traffic_data.loc[date_range, hour] = traffic_data.loc[date_range, hour].round(2)  # 四舍五入保留两位小数
 
-# 将新数据转换为DataFrame并打印
-df = pd.DataFrame({'Hour': hours, 'Traffic': traffic_data_new})
-print(df)
-df.to_csv('history_data.csv', index=False)
+
+traffic_data.to_csv('history_data.csv', index=False)
