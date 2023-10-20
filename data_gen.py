50800e3e1f1f7c93c48c348be6f3d43639564d6d
lzy-a <1160440131@qq.com>
2023-10-20 15:12:43 +0800

    data

--- data_gen.py
+++ data_gen.py
@@ -1,27 +1,36 @@
-import numpy as np
 import pandas as pd
+import numpy as np
+
+# 创建日期范围
+date_range = pd.date_range(start='1/1/2023', end='12/31/2023', freq='T')
+
+# 初始化一个全0的序列
+data = np.zeros(len(date_range))
+
+# 设置每天的1:00 - 7:00范围内的数值大小在1000左右10%波动
+for i in range(len(date_range)):
+    if date_range[i].hour >= 1 and date_range[i].hour < 7:
+        data[i] = 1000 + np.random.uniform(-100, 100)
+
+# 设置每天的其他时间段数值大小在6000左右10%波动
+for i in range(len(date_range)):
+    if date_range[i].hour < 1 or date_range[i].hour >= 7:
+        data[i] = 6000 + np.random.uniform(-600, 600)
+
+# 设置每天的1000到6000以及6000到1000的流量波动，在一小时内快速上升和下降
+for i in range(len(date_range)):
+    if date_range[i].minute == 0:
+        if date_range[i].hour == 1 or date_range[i].hour == 7:
+            data[i:i+60] = np.linspace(data[i], data[i+60], 60)
+
+# 在某些分钟（约占1%）出现30%-50%的流量激增
+increase_indices = np.random.choice(len(date_range), size=int(0.01*len(date_range)), replace=False)
+for i in increase_indices:
+    data[i] *= np.random.uniform(1.3, 1.5)
+
+# 创建一个pandas Series对象
+data_series = pd.Series(data, index=date_range)
 
 
-# 定义全天的24小时
-hours = np.arange(24)
 
-# 设置一年日期范围
-date_range = pd.date_range(start='1/1/2023', end='12/31/2023')
-
-# 初始化一个空的DataFrame用于存储流量数据
-traffic_data = pd.DataFrame(index=date_range)
-
-# 为每天的1-7点设置流量为1000，其他时间段流量为6000
-for hour in range(24):
-    if hour < 7:
-        traffic_data.loc[date_range, hour] = 1000
-    else:
-        traffic_data.loc[date_range, hour] = 6000
-
-    # 对全时段应用10%的随机波动
-for hour in range(24):
-    traffic_data.loc[date_range, hour] = traffic_data.loc[date_range, hour] * (1 + np.random.rand(len(date_range)) / 10)
-    traffic_data.loc[date_range, hour] = traffic_data.loc[date_range, hour].round(2)  # 四舍五入保留两位小数
-
-
-traffic_data.to_csv('history_data.csv', index=False)
+data_series.to_csv('history_data.csv', index=False)
--- main.py
+++ main.py
@@ -6,25 +6,50 @@ from lstm import LSTMModel
 from torch.utils.data import Dataset, DataLoader
 
 
-class CustomDataset(Dataset):
-    def __init__(self, data):
-        self.data = data
+def generate_sequences(df: pd.DataFrame, tw: int, pw: int, target_columns, drop_targets=False):
+    '''
+   df: Pandas DataFrame of the univariate time-series
+   tw: Training Window - Integer defining how many steps to look back
+   pw: Prediction Window - Integer defining how many steps forward to predict
+
+   returns: dictionary of sequences and targets for all sequences
+   '''
+    data = dict()  # Store results into a dictionary
+    L = len(df)
+    for i in range(L - tw):
+        # Option to drop target from dataframe
+        if drop_targets:
+            df.drop(target_columns, axis=1, inplace=True)
+
+        # Get current sequence
+        sequence = df[i:i + tw].values
+        # Get values right after the current sequence
+        target = df[i + tw:i + tw + pw][target_columns].values
+        data[i] = {'sequence': sequence, 'target': target}
+    return data
+
+
+class SequenceDataset(Dataset):
+
+    def __init__(self, df):
+        self.data = df
+
+    def __getitem__(self, idx):
+        sample = self.data[idx]
+        return torch.Tensor(sample['sequence']), torch.Tensor(sample['target'])
 
     def __len__(self):
         return len(self.data)
 
-    def __getitem__(self, idx):
-        return self.data[idx]
 
 date_range = pd.date_range(start='1/1/2023', end='12/31/2023')
 traffic_data = pd.read_csv('history_data.csv')
-data = torch.tensor(traffic_data.values)
+data = generate_sequences(traffic_data, tw=24, pw=24, target_columns=[1])
 
-dataset = CustomDataset(data)
+dataset = SequenceDataset(data)
 dataloader = DataLoader(dataset, batch_size=32, shuffle=True)
 
-
-input_size = data.shape[1]  # 输入特征的维度（这里假设为1）
+input_size = data.shape[1]
 hidden_size = 100  # LSTM隐藏层的大小
 output_size = 1  # 输出特征的维度（这里假设为1）
 num_epochs = 100  # 训练的轮数
@@ -37,14 +62,13 @@ optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)  # Adam优化
 # 训练模型
 for epoch in range(num_epochs):
     for batch in dataloader:
+        print(batch)
         # 前向传播
         outputs = model(batch)
-        loss = criterion(outputs, torch.tensor([1]*len(batch)))
+        loss = criterion(outputs, torch.tensor([1] * len(batch)))
 
         # 反向传播和优化
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
         print(f'Epoch [{epoch + 1}/{num_epochs}], Loss: {loss.item():.4f}')
-
-
