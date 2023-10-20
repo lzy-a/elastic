790e0c82253b8c54b13324b546f1fde5cc739652
lzy-a <1160440131@qq.com>
2023-10-20 16:33:19 +0800

    lstm

--- main.py
+++ main.py
@@ -87,10 +87,11 @@ if __name__ == '__main__':
             # Forward Pass
             preds = model(x).squeeze()
             loss = criterion(preds, y)  # compute batch loss
+            print(f'loss: {loss.item()}')
             train_loss += loss.item()
             loss.backward()
             optimizer.step()
-            print(f'loss: {loss.item()}')
+
         epoch_loss = train_loss / len(trainloader)
         t_losses.append(epoch_loss)
 
