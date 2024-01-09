65b3c3d2ef3826fe228e5a20820a26c09851247d
lzy-a <1160440131@qq.com>
2024-01-09 17:26:48 +0800

    dist barrier

--- ddp_deepfm_test.py
+++ ddp_deepfm_test.py
@@ -163,15 +163,13 @@ if __name__ == "__main__":
             optimizer.zero_grad()
             loss = loss_func(y_hat, y)
             loss.backward()
-            #dist.barrier()
-            #torch.cuda.synchronize()
+            torch.cuda.synchronize()
             loss_time = time.time() - loss_start
             loss_total += loss_time
 
             optimizer_start = time.time()
             optimizer.step()
-            #dist.barrier()
-            #torch.cuda.synchronize()
+            torch.cuda.synchronize()
             optimizer_time = time.time() - optimizer_start
             optimizer_total += optimizer_time
 
