e746d27764897ff0678284d3662f32b5d3037c7d
lzy-a <1160440131@qq.com>
2024-03-13 20:08:47 +0800

    test

--- data_provider/data_factory.py
+++ data_provider/data_factory.py
@@ -16,9 +16,10 @@ def data_provider(args, flag):
 
     if flag == 'test':
         shuffle_flag = False
-        drop_last = True
+        drop_last = False
         batch_size = 1
         freq = args.detail_freq
+        Data = Dataset_Pred
     elif flag == 'pred':
         shuffle_flag = False
         drop_last = False
