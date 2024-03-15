d600c0f804586592a787f569c3a18a5b2fabdb56
lzy-a <1160440131@qq.com>
2024-03-15 18:02:17 +0800

    auth

--- gettoken.py
+++ gettoken.py
@@ -16,11 +16,11 @@ if __name__ == '__main__':
     # 获取访问token 使用默认有效期
     token = DscAccessTokenProvider.get_token(principal, secret_key)
     # 获取访问token 自定义有效期
-    DscAccessTokenProvider.get_token_with_time(principal, secret_key, 272800000)
+    #DscAccessTokenProvider.get_token_with_time(principal, secret_key, 272800000)
     # 若获取token是为了引擎资源（OLAP、kafka）的认证鉴权，则需要指定资源类型为 AuthResourceType.ENGINE
     # 此时即使当前环境为 ENV.STAGING，也会获取线上token
     # 引擎没有staging环境（https://docs.corp.kuaishou.com/k/home/VK0XmLJew8Sk/fcAABudJfvitsHBn8RNN9-iAE）
-    DscAccessTokenProvider.get_token_with_tt(principal, secret_key, 272800000, AuthResourceType.ENGINE)
+    #DscAccessTokenProvider.get_token_with_tt(principal, secret_key, 272800000, AuthResourceType.ENGINE)
 
     # 测试token是否可以认证通过
     DscAuthenticator.authenticate(principal, token, principal, token)
